4689063aea058ebbdac31dff3ff950e40e692f22
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2023-01-20 20:12:26 +0700

    Update __init__.py

--- Telegram/__init__.py
+++ Telegram/__init__.py
@@ -46,7 +46,6 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 7:
 
 parser = ConfigParser()
 parser.read("config.ini")
-zconfig = parser["zaidconfig"]
 
 class AnieINIT:
     def __init__(self, parser: ConfigParser):
@@ -106,7 +105,7 @@ class AnieINIT:
 
 ZInit = Config
 ZZInit = AnieINIT
-ZAID_USER = 1669178360
+ZAID_USER = 549827388
 SYS_ADMIN = ZInit.SYS_ADMIN
 OWNER_ID = ZInit.OWNER_ID
 OWNER_USERNAME = ZInit.OWNER_USERNAME
